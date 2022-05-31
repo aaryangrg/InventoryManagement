5cffae92ce60360d06ae39795243924c125275f8
Aaryan Garg <aaryan.garg1@gmail.com>
2022-05-31 15:37:27 +0530

    Settings Changes

--- InventoryManagement/settings.py
+++ InventoryManagement/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dxjhh=i6wj83+y!p4ll@4*gktv_l^0(!)#q72fj19i%9b@49se'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["ims-sutt.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
